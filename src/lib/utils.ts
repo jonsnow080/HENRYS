101edb41511c4f34e95c83716504e4acc8dc4b31
jonsnow080 <151989918+jonsnow080@users.noreply.github.com>
2025-12-01 12:42:13 -0800

    fix: force production domain in production env

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -31,6 +31,7 @@ export function formatCurrency(amountInMinorUnits: number, currency: string) {
 export function getBaseUrl() {
   if (process.env.NEXT_PUBLIC_SITE_URL) return process.env.NEXT_PUBLIC_SITE_URL;
   if (process.env.SITE_URL) return process.env.SITE_URL;
+  if (process.env.VERCEL_ENV === "production") return "https://www.henrysdating.com";
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return "http://localhost:3000";
 }
